68450ae803f32ac7c28c6f5205bb764756961e6a
TiagoRodrigues456 <trodrigues322@gmail.com>
2021-04-07 05:54:39 -0300

    Configurando MirageJs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs'
 import { App } from './App';
+
+createServer({
+  routes () {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 'deposit',
+          category: 'Car',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 
 ReactDOM.render(
